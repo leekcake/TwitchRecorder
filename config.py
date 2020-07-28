3673a9763f1a10344d962171c86a96e45b446886
Blue Electric <leekcake@protonmail.com>
2020-07-28 20:12:48 +0900

    Move Recorder to sub folder

--- config.py
+++
@@ -1,9 +0,0 @@
-OUTPUT_GDRIVE = 0
-OUTPUT_DISK = 1
-
-outputTo = OUTPUT_DISK
-chunkSize = 1024 * 1024 * 100
-
-diskWarningQuota = 1024 * 1024 * 1024 * 100
-diskWarningIFTTT = "FILL_YOUR_MAKER_WEBHOOK_KEY"
-diskUploadQuota = 1024 * 1024 * 1024 * 190
---
+++ recorder/config.py
@@ -0,0 +1,9 @@
+OUTPUT_GDRIVE = 0
+OUTPUT_DISK = 1
+
+outputTo = OUTPUT_DISK
+chunkSize = 1024 * 1024 * 100
+
+diskWarningQuota = 1024 * 1024 * 1024 * 75
+diskWarningIFTTT = "daGi2z9itiSMoKI92KhKgQ"
+diskUploadQuota = 1024 * 1024 * 1024 * 150
